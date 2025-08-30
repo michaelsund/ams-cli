017009ed499a68f8f31b5463129369aa8b32c615
Michael Sund <michael@osund.com>
2025-08-30 22:45:21 +0200

    first commit

---
+++ src/amslib.rs
@@ -0,0 +1,78 @@
+use chrono::{DateTime, Utc};
+use prettytable::Table;
+use reqwest::Client;
+use reqwest::Error;
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AmsDataItem {
+    pub id: String,
+    pub internal_id: Option<usize>,
+    pub published_date: String,
+    pub title: String,
+    pub workplace: String,
+    pub workplace_name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct AmsData {
+    pub ads: Vec<AmsDataItem>,
+}
+
+pub(crate) async fn run(num_adverts: &String) -> AmsData {
+    let json_res = post_data(&num_adverts).await;
+    let ads: AmsData = serde_json::from_str(&json_res.unwrap()).unwrap();
+    display_pretty_table(&ads);
+    ads
+}
+
+fn display_pretty_table(data: &AmsData) {
+    let mut table = Table::new();
+
+    table.add_row(row![FY => "Id", "Date", "Place", "Company", "Title"]);
+    for (i, ad) in data.ads.iter().enumerate() {
+        let id = i + 1;
+        let date = parse_date(ad.published_date.clone());
+        let place = &ad.workplace;
+        let company = &ad.workplace_name;
+        let title = &ad.title;
+        table.add_row(row![
+            FY -> &id.to_string(),
+            &date.format("%d/%m"),
+            &place,
+            &company,
+            &title
+        ]);
+    }
+
+    table.printstd();
+}
+
+fn parse_date(date: String) -> DateTime<Utc> {
+    date.parse().expect("Failed to parse datetime")
+}
+
+async fn post_data(num_adverts: &String) -> Result<String, Error> {
+    let client = Client::new();
+    let res = client
+        .post("https://platsbanken-api.arbetsformedlingen.se/jobs/v1/search")
+        .json(&json!({
+            "filters": [
+                { "type": "occupationField", "value": "apaJ_2ja_LuF" },
+                { "type": "region", "value": "xTCk_nT5_Zjm" }
+            ],
+            "fromDate": "",
+            "order": "relevance",
+            "maxRecords": &num_adverts,
+            "startIndex": 0,
+            "toDate": "2025-08-29T16:53:30.326Z",
+            "source": "pb"
+        }))
+        .send()
+        .await?;
+    let json = res.text().await.unwrap();
+
+    Ok(json)
+}
---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use clap::{Command, arg};
+use std::io::Write;
+use tokio::io::Error;
+mod amslib;
+use amslib::AmsData;
+#[macro_use]
+extern crate prettytable;
+
+#[tokio::main]
+async fn main() {
+    let matches = Command::new("ams")
+        .version("1.0")
+        .about("Fetches and displays IT jobadverts from arbetsformedlingen.se in Örebro")
+        .arg(
+            arg!(--numadverts <VALUE>)
+                .required(false)
+                .default_value("15"),
+        )
+        .get_matches();
+
+    let num_adverts = matches.get_one::<String>("numadverts").unwrap();
+
+    // Call
+    let data: AmsData = amslib::run(&num_adverts).await;
+    loop {
+        print!("> ");
+        std::io::stdout().flush().unwrap();
+        let inp = read_input().expect("Problem reading input");
+        let selected_index: usize = inp
+            .trim()
+            .parse()
+            .expect("Failed to parse selected index-string to usize");
+        // TODO: this could be handled the Rust way?
+        if selected_index <= data.ads.len() + 1 && selected_index != 0 {
+            // Open the link
+            let combined_url = "https://arbetsformedlingen.se/platsbanken/annonser/".to_owned()
+                + &data.ads[selected_index - 1].id;
+            // TODO: Check result here if the browser could not be opened.
+            open_browser_link(combined_url).expect("Problem opening browser!");
+        } else {
+            println!("Index out of range!")
+        }
+    }
+}
+
+fn read_input() -> Result<String, Error> {
+    let mut buffer = String::new();
+    let stdin = std::io::stdin();
+    stdin.read_line(&mut buffer)?;
+    Ok(buffer)
+}
+
+fn open_browser_link(url: String) -> Result<(), Error> {
+    println!("Opening link {}", url);
+    open::that(url)?;
+    Ok(())
+}
